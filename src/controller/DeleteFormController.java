9df2f47aedcc53949c742fe0cd29698371a06189
nadeeshanka <mailtosiridesha@gmail.com>
2019-11-05 23:14:02 +0530

    Add delete form and Contraller

--- src/controller/DeleteFormController.java
+++ src/controller/DeleteFormController.java
@@ -1,10 +1,12 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -15,6 +17,7 @@ import java.util.ResourceBundle;
 
 public class DeleteFormController implements Initializable {
 
+    public TableView tbl_remove;
     @FXML
     private AnchorPane root;
 
@@ -32,5 +35,9 @@ public class DeleteFormController implements Initializable {
         Stage primaryStage = (Stage) (this.root.getScene().getWindow());
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
+    }
+
+    public void btn_delete_onaction(ActionEvent actionEvent) {
+
     }
 }
