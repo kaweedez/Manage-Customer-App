4f9846d22b97e9947dd59620b13949c1335b5849
LeelK <karunarathne1995@gmail.com>
2019-11-05 22:57:36 +0530

    search ui add

--- src/controller/SearchFormController.java
+++ src/controller/SearchFormController.java
@@ -1,12 +1,16 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import util.CustomerTM;
 
 import java.io.IOException;
 import java.net.URL;
@@ -14,6 +18,8 @@ import java.util.ResourceBundle;
 
 public class SearchFormController implements Initializable {
     public AnchorPane root;
+public JFXTextField txtSearch;
+    public TableView<CustomerTM> tblCustomer;
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
         URL resource = this.getClass().getResource("/view/DashboardForm.fxml");
@@ -26,6 +32,15 @@ public class SearchFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+
+        tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("cusId"));
+        tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("cusName"));
+        tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("cusAddress"));
+        tblCustomer.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("cusContact"));
+
+
+
 
     }
 }
